8f97553608bd971270c7abdf2f7b4c6b23a69295
unknown <st7692833267@gamil.com>
2020-10-10 16:45:57 +0530

    video chat

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+  const config = {
+    apiKey: "AIzaSyDSNgLBjQPKKFXXa0yR77gsHVroOTbOoe4",
+    authDomain: "shivamchat-87975.firebaseapp.com",
+    databaseURL: "https://shivamchat-87975.firebaseio.com",
+    projectId: "shivamchat-87975",
+    storageBucket: "shivamchat-87975.appspot.com",
+    messagingSenderId: "542213401089",
+    appId: "1:542213401089:web:dc1c7abf883a751f73ae00",
+    measurementId: "G-1PSGZV5VQ1"
+  };
+
+  export default config
